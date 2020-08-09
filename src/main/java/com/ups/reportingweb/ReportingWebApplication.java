54a4198beee9c5531c610403678c2b14f1a41c05
Arjun Moorthy <amoorthy@ups.com>
2020-08-09 17:21:40 +0530

    My First Commit

---
+++ src/main/java/com/ups/reportingweb/ReportingWebApplication.java
@@ -0,0 +1,13 @@
+package com.ups.reportingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ups/reportingweb/ReportingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ups.reportingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
